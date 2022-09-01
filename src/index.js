81b09490f21374dcd6fd8fbf92f9df9aab863a47
Shubhra Sachdev <shubhra99sachdev@gmail.com>
2022-09-01 15:30:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,3 +9,15 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// function tick() {
+//   const element = 
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   ;
+//   root.render(element);
+// }
+
+// setInterval(tick, 1000);
